21aa81889c00b7470467b9194af3d75865899cb5
Tony Georgiadis <tony.georgiadis@acuris.com>
2018-12-24 16:53:14 +0000

    Save react files locally .. path to repo to be parameterized

--- download-react.sh
+++ download-react.sh
@@ -2,4 +2,17 @@
 
 echo "Downloading React $@"
 
+#echo "What is the location of the mmg-cdn-shared-libraries repo in your machine?"
+#read -e LOCATION
+#
+#echo "Location is $LOCATION"
+
 node index.js $@
+
+mkdir ~/Desktop/CodeAcuris/mmg-cdn-shared-libraries/cdn/react/$@
+cp ./react.js ~/Desktop/CodeAcuris/mmg-cdn-shared-libraries/cdn/react/$@
+cp ./react.min.js ~/Desktop/CodeAcuris/mmg-cdn-shared-libraries/cdn/react/$@
+
+mkdir ~/Desktop/CodeAcuris/mmg-cdn-shared-libraries/cdn/react-dom/$@
+cp ./react-dom.js ~/Desktop/CodeAcuris/mmg-cdn-shared-libraries/cdn/react-dom/$@
+cp ./react-dom.min.js ~/Desktop/CodeAcuris/mmg-cdn-shared-libraries/cdn/react-dom/$@
